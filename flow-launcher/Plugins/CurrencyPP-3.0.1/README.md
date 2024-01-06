dc3845a7aba95ff8d4981a20df982a60f95b44d6
Denys Chernyshov <den.blackshov@gmail.com>
2024-01-06 12:36:42 +0100

    Update

---
+++ flow-launcher/Plugins/CurrencyPP-3.0.1/README.md
@@ -0,0 +1,123 @@
+# Flow.Launcher.Plugin.CurrencyPP
+
+![demo](./demo.png)
+![settings](./settings.png)
+
+A port of [Keypirinha Currency plugin](https://github.com/AvatarHurden/keypirinha-currency) to Flow launcher.
+
+Compared to the existing [Currency Converter](https://github.com/deefrawley/Flow.Launcher.Plugin.Currency) plugin:
+ - Support [more currencies](https://docs.openexchangerates.org/reference/supported-currencies)
+ - Output multiple currencies
+ - Allow setting default currencies to convert from/to
+ - Customizable aliases. For example, 1$ can be configured to be USD or AUD
+ - Support math (see below)
+
+Below is an excerpt the original readme
+
+---
+
+## Usage
+
+For the most basic usage, simply enter the amount to convert, the source currency and the destination currency, such as `5 USD in EUR`.
+You can perform mathematical operations for the source amount, such as `10*(2+1) usd in EUR`, and you can even perform some math on the resulting amount `5 usd in EUR / 2`.
+
+Furthermore, you can add (or subtract) multiple currencies together, such as `5 USD + 2 GBP in EUR`.
+You can also convert into multiple destination currencies, such as `5 USD in EUR, GBP`, and each conversion will be displayed as a separate result.
+
+If you omit the name of a currency, such as in `5 USD` or `5 in USD`, the plugin will use the default currencies specified in the configuration file.
+You can also change what words and symbols are used between multiple destination currencies and between the source and destination.
+
+### Aliases
+
+By default, the plugin operates only on [ISO currency codes](https://pt.wikipedia.org/wiki/ISO_4217) (and a few others).
+However, there is support for *aliases*, which are alternative names for currencies.
+In the configuration file, the user can specify as many aliases as they desire for any currency (for instance, `dollar` and `dollars` for USD).
+Aliases, just like regular currency codes, are case-insensitive (i.e. `EuR`, `EUR` and `eur` are all treated the same).
+
+
+### Math
+
+The available mathematical operations are addition (`+`), subtraction (`-`), multiplication (`*`), division (`/`) and exponentiation (`**` or `^`).
+You can also use parentheses and the negative operator (`-(3 + 4) * 4`, for example).
+
+### Grammar
+
+For those familiar with BNF grammars and regex, below is grammar accepted by the parser (`prog` is the top-level expression):
+
+```
+prog := sources (to_key? destinations)? extra?
+
+to_key := 'to' | 'in' | ':'
+
+destinations := cur_code sep destinations | cur_code
+
+sep := ',' | '&' | 'and'
+cur_code := ([^0-9\s+-/*^()]+)
+  # excluding any words that are used as 'sep' or 'to_key'
+
+extra := ('+' | '-' | '*' | '/' | '**' | '^' ) expr
+
+sources := source ('+' | '-') sources | source
+source := '(' source ')'
+      | cur_code expr
+      | expr (cur_code?)
+
+expr := add_expr
+add_expr := mult_expr | add_expr ('+' | '-') mult_expr
+mult_expr := exp_expr | mult_expr ('*' | '/') exp_expr
+exp_expr := unary_expr | exp_expr ('^' | '**') unary_expr
+unary_expr := operand | ('-' | '+') unary_expr
+operand := number | '(' expr ')'
+
+number := (0|[1-9][0-9]*)([.,][0-9]+)?([eE][+-]?[0-9]+)?
+```
+
+## Backend
+
+The Currency plugin uses [OpenExchangeRates](https://openexchangerates.org/) to obtain hourly exchange rates for all currencies. Since this project does not make any money, it is using the free tier, which only allows 1000 requests per month. In order to allow the most number of people to use the plugin without any work, there is a cache layer that reduces the number of requests to the backend. 
+
+If this cache layer fails, however, the plugin quickly runs into this request limit. In order to work around this issue, the plugin allows users to specify their own App ID to use whenever the cache is older than 2 hours. This shouldn't happen often, but is a safeguard in case things go wrong. Users can get a free App ID by creating an account [here](https://openexchangerates.org/signup/free).
+
+## Change Log
+
+### v2.2
+* Added workaround for situations in which the cache fails.
+
+### v2.1
+
+* Improved grammar for more intuitive use
+* Bug fixes
+* Improved options to copy results to clipboard
+
+### v2.0
+
+* Improved parser. More flexible, and now you can specify your own separators in the config file
+* Math! Add, subtract, multiply, or divide numbers to obtain the source amount for a currency (also supports parentheses and exponents)
+* Multiple source currencies. Add or subtract amounts in different currencies to obtain a final result
+* An icon
+* Support for aliases. The user can create aliases ('nicknames') for any valid currency in the config file
+
+
+### v1.4
+
+* Added a layer between clients and OpenExchangeRates to mitigate API usage
+
+### v1.3
+
+* Changed API from Yahoo Finance to OpenExchangeRates
+
+### v1.2
+
+* Saves exchange information locally, updating automatically or manually
+* Allow converting currencies directly in the search
+
+### v1.1
+
+* Allow decimal amounts to be inserted (using either a comma or a period)
+* Added copy actions
+* Added configuration for default currencies
+* Multiple source and destination currencies can be specified
+
+### v1.0
+
+* Initial Release
---
+++ flow-launcher/Plugins/CurrencyPP-3.0.1/src/currencypp.py
@@ -0,0 +1,188 @@
+from exchange import ExchangeRates, UpdateFreq, CurrencyError
+from flox.utils import cache_path
+from parsy import ParseError
+from currencyparser import make_parser, ParserProperties
+from flox import Flox, clipboard
+
+
+class CurrencyPP(Flox):
+
+    broker = None
+
+    def __init__(self):
+        super().__init__()
+        # self.logger_level("debug")
+        self._read_config()
+
+        # actions = [
+        #     self.create_action(
+        #         name=self.ACTION_COPY_RESULT,
+        #         label="Copy result with code",
+        #         short_desc="Copy result (with code) to clipboard"),
+        #     self.create_action(
+        #         name=self.ACTION_COPY_AMOUNT,
+        #         label="Copy numerical result",
+        #         short_desc="Copy numerical result to clipboard"),
+        #     self.create_action(
+        #         name=self.ACTION_COPY_EQUATION,
+        #         label="Copy conversion",
+        #         short_desc="Copy conversion equation to clipboard")]
+
+        # self.set_actions(self.ITEMCAT_RESULT, actions)
+
+    def query(self, user_input):
+        try:
+            query = self._parse_and_merge_input(user_input, True)
+            # This tests whether the user entered enough information to
+            # indicate a currency conversion request.
+            if not self._is_direct_request(query):
+                return
+            # if the conversion would have failed, return now
+            self.broker.convert(self._parse_and_merge_input(user_input))
+        except CurrencyError:
+            return
+        except Exception as e:
+            # self.logger.error("convert error:\n" +
+            #                   "\n".join(traceback.format_exception(e)))
+            return
+
+        try:
+            query = self._parse_and_merge_input(user_input)
+            if query['destinations'] is None or query['sources'] is None:
+                return
+
+            if self.broker.tryUpdate():
+                self._update_update_item()
+
+            if self.broker.error:
+                self.add_item("Webservice failed",
+                              '{}'.format(self.broker.error))
+            else:
+                results = self.broker.convert(query)
+
+                for result in results:
+                    self.add_item(
+                        result['title'],
+                        result['description'],
+                        context=result['description'],
+                        method=self.item_action,
+                        parameters=[result['amount']],
+                        score=100,
+                    )
+        except Exception as exc:
+            self.add_item("query", "Error: " + str(exc))
+
+        self.add_item(
+            'Update Currency',
+            'Last updated at ' + self.broker.last_update.isoformat(),
+            method=self.update_rates,
+            parameters=[user_input],
+            dont_hide=True
+        )
+
+    def item_action(self, amount):
+        clipboard.put(str(amount))
+
+    def update_rates(self, last_query):
+        self.broker.update()
+        self.change_query(str(last_query), True)
+
+    def _is_direct_request(self, query):
+        entered_dest = ('destinations' in query and
+                        query['destinations'] is not None)
+        entered_source = (query['sources'] is not None and
+                          len(query['sources']) > 0 and
+                          query['sources'][0]['currency'] is not None)
+
+        return entered_dest or entered_source
+
+    def _parse_and_merge_input(self, user_input=None, empty=False):
+        if empty:
+            query = {'sources': None}
+        else:
+            query = {
+                'sources': [{'currency': self.broker.default_cur_in, 'amount': 1.0}],
+                'destinations': [{'currency': cur} for cur in self.broker.default_curs_out],
+                'extra': None
+            }
+
+        if not user_input:
+            return query
+
+        user_input = user_input.lstrip()
+
+        try:
+            parsed = self.parser.parse(user_input)
+            if not parsed['destinations'] and 'destinations' in query:
+                parsed['destinations'] = query['destinations']
+            return parsed
+        except ParseError:
+            return query
+
+    def _read_config(self):
+        def _warn_cur_code(name, fallback):
+            fmt = "Invalid {} value in config. Falling back to default: {}"
+            self.logger.warning(fmt.format(name, fallback))
+
+        self.update_freq = UpdateFreq(self.settings.get('update_freq'))
+
+        app_id_key = self.settings.get('app_id').strip()
+
+        cache_dir = cache_path(self.name)
+        cache_dir.mkdir(exist_ok=True)
+        self.broker = ExchangeRates(
+            cache_dir, self.update_freq, app_id_key, self)
+
+        input_code = self.settings.get('input_cur').strip()
+        validated_input_code = self.broker.set_default_cur_in(input_code)
+
+        if not validated_input_code:
+            _warn_cur_code("input_cur", self.broker.default_cur_in)
+
+        output_code = self.settings.get('output_cur').strip()
+        validated_output_code = self.broker.set_default_curs_out(output_code)
+
+        if not validated_output_code:
+            _warn_cur_code("output_cur", self.broker.default_curs_out)
+
+        # separators
+        separators_string = self.settings.get('separators').strip()
+        separators = separators_string.split()
+
+        # destination_separators
+        dest_seps_string = self.settings.get('destination_separators').strip()
+        dest_separators = dest_seps_string.split()
+
+        # aliases
+        self.broker.clear_aliases()
+
+        aliases_string = self.settings.get('aliases')
+
+        for line in aliases_string.splitlines():
+            try:
+                key, aliases_string = line.split('=')
+                key = key.strip()
+                aliases_string = aliases_string.strip()
+
+                validatedKey = self.broker.validate_code(key)
+                aliases = aliases_string.split()
+
+                for alias in aliases:
+                    validated = self.broker.validate_alias(alias)
+                    if validated:
+                        self.broker.add_alias(validated, validatedKey)
+                    else:
+                        self.logger.warning(
+                            'Alias {} is invalid. It will be ignored'.format(alias))
+            except Exception:
+                self.logger.warning(
+                    'Key {} is not a valid currency. It will be ignored'.format(key))
+
+        properties = ParserProperties()
+        properties.to_keywords = separators
+        properties.sep_keywords = dest_separators
+        self.parser = make_parser(properties)
+
+
+if __name__ == "__main__":
+    CurrencyPP()
