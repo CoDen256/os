44059e668abd25950b2d9991221fb607c3268453
Denys <den.blackshov@gmail.com>
2025-01-15 22:33:36 +0100

    add vimium, remove stuff

--- flow-launcher/dev/Plugins/DWDS-Flow-2.1.0/dev.ps1
+++
@@ -1,7 +0,0 @@
-Set-Variable -Name "FLOW_HOME" (Get-ChildItem -Dir -Path "~\scoop\apps\flow-launcher\current" -Filter 'app*').FullName
-Remove-Item -Path "$FLOW_HOME\UserData\Plugins\DWDS-Flow-*" -Recurse -Force
-New-Item -Path "$FLOW_HOME\UserData\Plugins\DWDS-Flow-Dev" -ItemType Junction -Value $PWD\..\dwds-api -Force
-pip install -r "$FLOW_HOME\UserData\Plugins\DWDS-Flow-Dev\requirements.txt" -t "$FLOW_HOME\UserData\Plugins\DWDS-Flow-Dev\lib"
-
-taskkill /f /im Flow*
-Start-Process -FilePath "~\scoop\apps\flow-launcher\current\Flow.Launcher.exe"
