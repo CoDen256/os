ca1ab5a9ff320e1ee4296c20a723c88682ce2744
Denys Chernyshov <den.blackshov@gmail.com>
2025-01-19 14:56:52 +0100

    add kitty conf

--- cfg/init.xsh
+++ cfg/init.xsh
@@ -4,4 +4,4 @@ if not !(which stow):
     print("No stow. Installing")
     sudo apt-get install -y stow
 
-stow --dir=~/os/cfg --target=~/ git yazi
+stow --dir=~/os/cfg --target=~/ git yazi kitty xonsh
---
+++ linux/bootstrap.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade
+
+# wifi drivers Alpha AWUS https://github.com/morrownr/8814au
+sudo apt install -y linux-headers-$(uname -r) build-essential bc dkms git libelf-dev rfkill iw
+git clone https://github.com/morrownr/8814au.git
+cd 8814au
+sudo ./install-driver.sh
+sudo dkms status
+cd
+rm -rf 8814au
+
+# this repo
+git clone git@github.com:CoDen256/os.git ~/os
+
+# xonsh-shell
+sudo apt install -y pipx
+pipx install 'xonsh[full]' pygments prompt-toolkit setproctitle   
+
+which xonsh | sudo tee -a /etc/shells
+chsh -s $(which xonsh) coden
+
+# mise
+curl https://mise.run | sh
+
+# config files
+xonsh ~/os/cfg/init.xsh
+
+# actual setup
+xonsh setup.xsh
