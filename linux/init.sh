b505596ac7f16ebd03fcd159da176c17648698ca
Denys Chernyshov <den.blackshov@gmail.com>
2025-01-28 00:21:54 +0100

    add bashrc

--- linux/init.sh
+++ linux/init.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash 
 
-stow --dir=$HOME/os/cfg --target=$HOME/ git yazi kitty xonsh ulauncher mise profile
+stow --dir=$HOME/os/cfg --target=$HOME/ git yazi kitty xonsh ulauncher mise profile ghostty
--- nixos/init.sh
+++ nixos/init.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash 
 
-stow --dir=$HOME/os/cfg --target=$HOME/ git yazi kitty xonsh ulauncher ghostty
+stow --dir=$HOME/os/cfg --target=$HOME/ git yazi kitty xonsh ulauncher ghostty bash
 
 sudo stow --dir=$HOME/os/cfg --target=/etc/ nix
