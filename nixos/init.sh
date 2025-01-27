6da30ba4a48a7454e13f07b5a013bea2ca1ceb10
Denys Chernyshov <den.blackshov@gmail.com>
2025-01-28 00:13:36 +0100

    add yazi and ghostty config

--- nixos/init.sh
+++ nixos/init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-stow --dir=$HOME/os/cfg --target=$HOME/ git yazi kitty xonsh ulauncher
+stow --dir=$HOME/os/cfg --target=$HOME/ git yazi kitty xonsh ulauncher ghostty
 
 sudo stow --dir=$HOME/os/cfg --target=/etc/ nix
