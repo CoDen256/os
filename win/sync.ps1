4917e2d6d4a019151b96f1da92faaa84e13228ae
Denys Chernyshov <den.blackshov@gmail.com>
2025-10-27 16:15:47 +0100

    update

--- win/sync.ps1
+++ win/sync.ps1
@@ -2,6 +2,16 @@ param([Parameter(Position=1, Mandatory=$false)]
 [string]$action="stow"
 )
 
-& "$PSScriptRoot\stow.ps1" $action -src $PSScriptRoot\..\cfg -dest $HOME yazi,wt
+& "$PSScriptRoot\stow.ps1" $action -src $PSScriptRoot\..\cfg -dest $HOME yazi,wt,starship
 & "$PSScriptRoot\stow.ps1" $action -src $PSScriptRoot\..\cfg -dest C:\\ ahk
+
 scoop update *
+
+
+# sync keymap
+
+
+# sync flow-launcher
+
+
+# sync scoop
