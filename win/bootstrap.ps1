2ccbfc6da5db3bb9da12458fd2f9188123347d2b
Denys <den.blackshov@gmail.com>
2025-12-04 12:01:43 +0100

    update

--- win/bootstrap.ps1
+++ win/bootstrap.ps1
@@ -5,13 +5,17 @@ Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
 Get-Service -Name ssh-agent | Set-Service -StartupType Automatic
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 
-
 ### Scoop and must-have apps
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
 scoop install git
 scoop bucket add extras
+
+### [AS ADMIN]  Install choco & choco packages system wide
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+
 
 ### Setup folder
 git clone git@github.com:CoDen256/os.git C:\os
@@ -21,16 +25,13 @@ git config user.email den.blackshov@gmail.com               # for this setup pro
 git config user.name Denys Chernyshov                       # for this setup project only
 
 
-### Install choco & choco packages system wide
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco feature enable -n allowGlobalConfirmation
-
-
 ### Choose flavour
-choco install C:\os\cfg\choco\dchernyshov.config # or other
 scoop install C:\os\cfg\scoop\dchernyshov.json # or other
+# [AS ADMIN] 
+choco install C:\os\cfg\choco\dchernyshov.config # or other
+
 
 ### Post install configuration by flavour
 
-### Run stow at the end of the install to link all the needed files from admin
-C:\os\win\stow.ps1
+### [AS ADMIN] Run stow at the end of the install to link all the needed files from admin
+C:\os\win\sync.ps1
